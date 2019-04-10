60b7816cc3f972bb97039acc512592ddabfdeeaa
adrianopc <adrianopc@ufcspa.edu.br>
2019-04-10 14:49:46 -0300

    criação projeto aula 6 meu primeiro projeto

---
+++ primeiroProjeto/src/main/java/xyz/cavalheiro/api/PrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package xyz.cavalheiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+
+}
